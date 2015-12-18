8cf557b655a3aed3effbadf54cfdf7141e6925fa
Ander Juaristi <ajuaristi@gmx.es>
2015-12-18 15:30:38 +0100

    Add support for additional source files
     * vimfiles/tools/shell/bash/update-filelist.sh: check variable
       'ADDITIONAL_FOLDERS'.
       (scan_folder): new function. Moved folder scanning code.
       Removed repeated code.

--- vimfiles/tools/shell/bash/update-filelist.sh
+++ vimfiles/tools/shell/bash/update-filelist.sh
@@ -16,20 +16,62 @@ fi
 # get filelist
 echo "  |- generate ${TMP}"
 
-if test "${FOLDERS}" != ""; then
-    find ${FORCE_POSIX_REGEX_1} . -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} ${IS_EXCLUDE} -regex ".*/("${FOLDERS}")/.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")$" > "${TMP}"
+# generate the argument list for find
+FIND_ARGS="${FORCE_POSIX_REGEX_1} -type f -not -path */\.* ${FORCE_POSIX_REGEX_2}"
 
-    if [[ "${FILE_SUFFIXS}" =~ __EMPTY__ ]]; then
-        find ${FORCE_POSIX_REGEX_1} . -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} ${IS_EXCLUDE} -regex ".*/("${FOLDERS}")/.*" ${FORCE_POSIX_REGEX_2}  |grep  -v  "\.\w*$" |xargs -i sh -c 'file="{}";type=$(file $file);[[ $type =~ "text" ]] && echo $file' >> "${TMP}"
+FOLDER_ARGS="${IS_EXCLUDE} -regex .*/("${FOLDERS}")/.* ${FORCE_POSIX_REGEX_2}"
+FILE_ARGS="-regex .*\.("${FILE_SUFFIXS}")$"
+
+scan_folder()
+{
+    if [ "$1" = "." ]; then
+        echo "  |- scanning working directory"
+    else
+        echo "  |- scanning '$1'"
+        if [ ! -d "$1" ]; then
+            echo "ERROR: '$1' is not a directory"
+            return 1
+        fi
     fi
 
-else
-    find ${FORCE_POSIX_REGEX_1} . -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")$" > "${TMP}"
-
-    if [[ "${FILE_SUFFIXS}" =~ __EMPTY__ ]]; then
-        find ${FORCE_POSIX_REGEX_1} . -type f -not -path "*/\.*" ${FORCE_POSIX_REGEX_2} -regex ".*\.("${FILE_SUFFIXS}")$" |grep  -v  "\.\w*$" |xargs -i sh -c 'file="{}";type=$(file $file);[[ $type =~ "text" ]] && echo $file' >> "${TMP}"
+    if test "$2" != ""; then
+        find $1 ${FIND_ARGS} ${FOLDER_ARGS} ${FILE_ARGS} > "${TMP}"
+        EXT="$1 ${FIND_ARGS} ${FOLDER_ARGS}"
+    else
+        find $1 ${FIND_ARGS} ${FILE_ARGS} > "${TMP}"
+        EXT="$1 ${FIND_ARGS} ${FILE_ARGS}"
     fi
 
+    if [[ "${FILE_SUFFIXS}" =~ __EMPTY__ ]]; then
+        find ${EXT} \
+        | grep  -v  "\.\w*$" \
+        | xargs -i sh -c 'file="{}";type=$(file $file);[[ $type =~ "text" ]] && echo $file' \
+        >> "${TMP}"
+    fi
+}
+
+# first, scan the working directory
+# apply the folder filters defined by the user
+scan_folder "." "${FOLDERS}"
+
+# now scan all the additional folders
+# these are assumed to be out of the project tree
+if [ -n "${ADDITIONAL_FOLDERS}" ]; then
+    INDEX="1"
+    CUR_DIR=""
+
+    while true
+    do
+        AWK_CMD="BEGIN { FS = \",\" } { print \$${INDEX} }"
+        CUR_DIR="$(echo ${ADDITIONAL_FOLDERS} | awk "${AWK_CMD}")"
+        if [ -n "${CUR_DIR}" ]; then
+            # we ignore folder filters here
+            scan_folder "${CUR_DIR}"
+            INDEX=$((${INDEX}+1))
+        else
+            break
+        fi
+    done
 fi
 
 # DISABLE
